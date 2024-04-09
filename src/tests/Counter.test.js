0b375720db57e64e9074bbc9484b2b3566fbe16c
RaySoftware <16505434+RaySoftware@users.noreply.github.com>
2024-04-09 19:32:49 -0400

    Add test cases

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,35 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import {render, screen} from '@testing-library/react'
+import Counter from "../components/Counter.js"
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />)
 })
 
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+test('renders counter message', async () => {
+  await screen.findByRole("heading");
+  expect(screen.getByRole("heading")).toHaveTextContent("Counter");
 });
 
-test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+test('should render initial count with value of 0', async () => {
+  await screen.findByTestId("count");
+  expect(screen.getByTestId("count")).toHaveTextContent("0");
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking + increments the count', async () => {
+  let countBefore = parseInt(screen.getByTestId("count").textContent);
+
+  userEvent.click(screen.getByRole("button", {name: "+"}));
+  expect(screen.getByTestId("count")).toHaveTextContent((countBefore + 1).toString());
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  let countBefore = parseInt(screen.getByTestId("count").textContent);
+  
+  userEvent.click(screen.getByRole("button", {name: "-"}));
+  expect(screen.getByTestId("count")).toHaveTextContent((countBefore - 1).toString());
 });
